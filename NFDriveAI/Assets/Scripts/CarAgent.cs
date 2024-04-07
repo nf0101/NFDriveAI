770a05b3ae8437ceeb018b948e7cf943517a7097
Nicola Frugieri <frugierinicola.ragg@gmail.com>
2024-04-07 16:27:43 +0200

    Agent implementation

---
+++ NFDriveAI/Assets/Scripts/CarAgent.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CarAgent : MonoBehaviour
+{
+    private float[,] qtable = new float[20,5];
+    private float learningRate = 0.5f;
+    private float discountFactor = 0.9f;
+    private float explorationStart = 0.1f;
+    private float explorationEnd = 0.01f;
+    private int finishLineReward = 100;
+    private int goodDrivingReward = 1;
+    private int badDrivingPenalty = -1;
+    private int wallPenalty = -100;
+    private float decayRate = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(GetExplorationRate(1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float GetExplorationRate(int currentEpisode)
+    {
+        float explorationRate = explorationStart * Mathf.Pow((explorationEnd / explorationStart), currentEpisode * decayRate);
+        return explorationRate;
+    }
+
+    int GetAction(int state, int currentEpisode)
+    {
+        float explorationRate = GetExplorationRate(currentEpisode);
+        float maxQValue = float.MinValue;
+        int bestAction = 0;
+
+        if (Random.Range(0.1f, 1.1f) > explorationRate)
+        {
+            return Random.Range(0, 6);
+        }
+        else
+        {
+            for (int action = 0; action < qtable.GetLength(1); action++)
+            {
+                if (qtable[state, action] > maxQValue)
+                {
+                    maxQValue= qtable[state, action];
+                    bestAction = action;
+                }
+            }
+            return bestAction;
+        }
+    }
+
+    float GetMaxQValue(int state)
+    {
+        float maxQValue = float.MinValue;
+        for (int action = 0; action < qtable.GetLength(1); action++)
+        {
+            if (qtable[state, action] > maxQValue)
+            {
+                maxQValue = qtable[state, action];
+            }
+        }
+
+        return maxQValue;
+    }
+
+    public void UpdateQTable(int state, int action, float reward, int nextState)
+    {
+        float maxNextQValue = GetMaxQValue(nextState);
+        float currentQValue = qtable[state, action];
+        float newQValue = currentQValue + learningRate * (reward + discountFactor * maxNextQValue - currentQValue);
+        qtable[state, action] = newQValue;
+    }
+
+    public void FinishEpisode(int currentEpisode, bool train = true)
+    {
+        int currentState = 0;
+    }
+}
--- NFDriveAI/Assets/Scripts/RayCasting.cs
+++ NFDriveAI/Assets/Scripts/RayCasting.cs
@@ -6,15 +6,11 @@ public class CapsuleCasting : MonoBehaviour
 {
     public float capsuleWidth = 0.2f;  // Larghezza della capsula
     public float capsuleHeight = 0.5f;  // Altezza della capsula
+    public float maxRaycastDistance = 1.5f;  // Lunghezza massima del raycast
 
     void Update()
     {
         // Calcola le posizioni degli estremi della capsula in base alla rotazione
-        //Vector2 leftEnd1 = transform.TransformPoint(new Vector2(-capsuleWidth / 2f, capsuleHeight / 2f));
-        //Vector2 leftEnd2 = transform.TransformPoint(new Vector2(-capsuleWidth / 2f, -capsuleHeight / 2f));
-        //Vector2 topEnd = transform.TransformPoint(new Vector2(0f, capsuleHeight / 2f));
-        //Vector2 bottomEnd = transform.TransformPoint(new Vector2(0f, -capsuleHeight / 2f));
-
         Vector2 leftEnd1 = transform.TransformPoint(new Vector2(capsuleWidth / 1.1f, -capsuleHeight / 2f));
         Vector2 leftEnd2 = transform.TransformPoint(new Vector2(-capsuleWidth / 1.1f, -capsuleHeight / 2f));
         Vector2 topEnd = transform.TransformPoint(new Vector2(0f, capsuleHeight / 2f));
@@ -25,22 +21,24 @@ public class CapsuleCasting : MonoBehaviour
         DrawRay(leftEnd2, transform.TransformDirection(Vector2.up));
 
         // Raggi laterali (sull'asse y)
-        DrawRay(topEnd, transform.TransformDirection(Vector2.left));
-        DrawRay(bottomEnd, transform.TransformDirection(Vector2.right));
+        //DrawRay(topEnd, transform.TransformDirection(Vector2.left));
+        //DrawRay(bottomEnd, transform.TransformDirection(Vector2.right));
     }
 
     void DrawRay(Vector2 start, Vector2 direction)
     {
-        RaycastHit2D hit = Physics2D.Raycast(start, direction);
+        RaycastHit2D hit = Physics2D.Raycast(start, direction, maxRaycastDistance);
 
         if (hit.collider != null)
         {
             Debug.DrawLine(start, hit.point, Color.red);
+            //print(hit.distance);
             //print(hit.collider.name);
         }
         else
         {
-            Debug.DrawRay(start, direction * 100f, Color.green);
+            Debug.DrawRay(start, direction * maxRaycastDistance, Color.green);
         }
+        
     }
 }
