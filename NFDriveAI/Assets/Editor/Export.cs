a78fe7f16049ff88a13cbe5aecca79e6ca10062d
Nicola Frugieri <frugierinicola.ragg@gmail.com>
2024-07-01 12:02:36 +0200

    Correzione package ed esportazione settings

---
+++ NFDriveAI/Assets/Editor/Export.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ExportPackage
+{
+    [MenuItem("Export/MyExport")]
+    static void export()
+    {
+        AssetDatabase.ExportPackage(AssetDatabase.GetAllAssetPaths(), PlayerSettings.productName + ".unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies | ExportPackageOptions.IncludeLibraryAssets);
+    }
+}
